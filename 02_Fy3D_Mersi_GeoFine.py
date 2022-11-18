6c777820acfb5b17a942baffe27e21720a9e371a
dc4win <dc1014549257dc@gmail.com>
2022-11-18 17:36:40 +0800

    20221118

---
+++ 02_Fy3D_Mersi_GeoFine.py
@@ -0,0 +1,47 @@
+import numpy as np
+from scipy.interpolate import interp2d
+import h5py
+
+GCP=[119.69048888,32.26087777]
+ImagePoint = [119.6907555,32.2763917]
+Distance = [GCP[0]-ImagePoint[0],GCP[1]-ImagePoint[1]]
+###Read Fy3 file
+filename = r"J:\ZJQX\zjqx_dc\Satellite\秸秆焚烧\风云3D火点监测\FY3D_MERSI_GBAL_L1_20221115_0530_0250M_MS(2).HDF"
+Handle  = h5py.File(filename, "r+")
+Lat = Handle["Geolocation/Latitude"].value
+Lon = Handle["Geolocation/Longitude"].value
+Lat_new =Lat +Distance[1]
+Lon_new=Lon + Distance[0]
+# fit_points = [np.linspace(0, 8200,410), np.linspace(0, 8000,400)]
+# [ut,vt]= [np.linspace(0, 8200,8200), np.linspace(0, 8000,8000)]
+# print(Lon.shape)
+#
+#
+# interp_lat = interp2d(fit_points[0],fit_points[1], Lat[:,:],kind="linear")
+# interp_lon = interp2d(fit_points[0],fit_points[1], Lon[:,:],kind="linear")
+# Lat_new = interp_lat(ut,vt)[:,:-8]
+# Lon_new = interp_lon(ut,vt)[:,:-8]
+# print(Lon_new.shape)
+del Handle["Geolocation/Latitude"],Handle["Geolocation/Longitude"]
+Lon = Handle.create_dataset("Geolocation/Longitude", data=Lon_new)
+Lat = Handle.create_dataset("Geolocation/Latitude", data=Lat_new)
+Handle.close()
+
+# Lon = Handle.select("Latitude")
+# def F(u, v):
+#     return u * np.cos(u * v) + v * np.sin(u * v)
+
+# fit_points = [np.linspace(0, 3, 8), np.linspace(0, 3, 8)]
+# print(*fit_points)
+#
+# values = F(*np.meshgrid(*fit_points, indexing='ij'))
+#
+# ut, vt = np.meshgrid(np.linspace(0, 3, 80), np.linspace(0, 3, 80), indexing='ij')
+#
+# true_values = F(ut, vt)
+#
+# test_points = np.array([ut.ravel(), vt.ravel()]).T
+#
+# interp = RegularGridInterpolator(fit_points, values)
+#
+# im = interp(test_points, method="linear").reshape(80, 80)
